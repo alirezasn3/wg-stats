b3dc63a15c5bf2241e8c6cd684b2c713b46418ac
Alireza Saadatnia <alirezasn3@gmail.com>
2023-05-27 04:14:16 +0330

    updated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -39,27 +39,29 @@ export default component$(() => {
     }, 1000);
   });
   return (
-    <div class="border-b-2 border-slate-600 flex flex-col justify-center items-center my-8 mx-4">
-      <div class="font-semibold">Total Usage</div>
-      <div class="flex my-2 text-green-500 text-sm md:text-lg font-bold">
-        <div>&#8595 {totalRx} GiB</div>
-        <div class="border-x-[1px] border-slate-600 mx-1 px-1">
-          &#8593 {totalTx} GiB
-        </div>
-        <div>&#8721 {total} GiB</div>
-      </div>
-      <div x-show="isAdmin[0]" class="font-semibold mt-4">
-        Current Usage
-      </div>
-      {isAdmin && (
+    <>
+      <div class="border-b-2 border-slate-600 flex flex-col justify-center items-center my-8 mx-4">
+        <div class="font-semibold">Total Usage</div>
         <div class="flex my-2 text-green-500 text-sm md:text-lg font-bold">
-          <>test</>
-          <div>&#8595 {totalRx} MiB</div>
-          <div class="border-l-[1px] border-slate-600 ml-1 pl-1">
-            &#8593 {totalTx} MiB
+          <div>&#8595 {totalRx} GiB</div>
+          <div class="border-x-[1px] border-slate-600 mx-1 px-1">
+            &#8593 {totalTx} GiB
           </div>
+          <div>&#8721 {total} GiB</div>
         </div>
-      )}
+        <div x-show="isAdmin[0]" class="font-semibold mt-4">
+          Current Usage
+        </div>
+        {isAdmin && (
+          <div class="flex my-2 text-green-500 text-sm md:text-lg font-bold">
+            <>test</>
+            <div>&#8595 {totalRx} MiB</div>
+            <div class="border-l-[1px] border-slate-600 ml-1 pl-1">
+              &#8593 {totalTx} MiB
+            </div>
+          </div>
+        )}
+      </div>
       {users.value.map((u, i) => (
         <div class="bg-slate-700 rounded mx-4 my-2 px-2 py-1 font-semibold">
           <span class="font-semibold">{i + 1}. </span>
@@ -79,7 +81,7 @@ export default component$(() => {
           </div>
         </div>
       ))}
-    </div>
+    </>
   );
 });
 
