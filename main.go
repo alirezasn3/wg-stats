ebad44303386d387581135e91edc94b6e86e3bd1
Alireza Saadatnia <alirezasn3@gmail.com>
2023-05-28 13:24:12 +0330

    updated

--- main.go
+++ main.go
@@ -6,56 +6,24 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
-	"sort"
 	"strconv"
 	"strings"
 	"time"
+
+	"golang.org/x/exp/slices"
 )
 
-var users []User
 var admins []string
 var Rx int
 var Tx int
+var peers map[string]*Peer = make(map[string]*Peer)
 
-type Database struct {
-	Path  string
-	Fd    *os.File
-	Users []User
-}
-
-func (db *Database) Init(path string) {
-	db.Path = path
-	data, err := os.ReadFile(path)
-	if err != nil {
-		panic(err)
-	}
-	json.Unmarshal(data, &db.Users)
-	db.Fd, err = os.Create(db.Path)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (db *Database) UpdateUserByName(name string, user User) {
-	for i := 0; i < len(db.Users); i++ {
-		if db.Users[i].Name == name {
-			db.Users[i] = user
-		}
-	}
-}
-
-func (db *Database) Commit() {
-	bytes, _ := json.Marshal(db.Users)
-	db.Fd.Write(bytes)
-}
-
-type User struct {
-	Name             string
-	Rx               int
-	Tx               int
-	LastestHandshake string
-	AllowedIps       string
-	ExpiresInDays    int
+type Peer struct {
+	Name            string
+	AllowedIps      string
+	LatestHandshake uint64
+	Rx              uint64
+	Tx              uint64
 }
 
 type bwOutput struct{}
@@ -86,98 +54,47 @@ func (c bwOutput) Write(p []byte) (int, error) {
 	return len(p), nil
 }
 
-func getUsers() ([]User, error) {
-	bytes, err := os.ReadFile("/etc/wireguard/wg0.conf")
+func updatePeersInfo() {
+	cmd := exec.Command("wg", "show", "wg0", "dump")
+	bytes, err := cmd.Output()
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
-	var clients map[string]string = make(map[string]string)
-	for strings.Contains(string(bytes), "###") {
-		i := strings.Index(string(bytes), "###")
-		bytes = bytes[i+3:]
-		i = strings.Index(string(bytes), "###")
-		if i == -1 {
-			i = len(bytes) - 1
+	for _, p := range strings.Split(strings.TrimSpace(string(bytes)), "\n")[1:] { // the first line is interface info
+		info := strings.Split(p, "\t")
+		if _, ok := peers[info[0]]; !ok {
+			peers[info[0]] = &Peer{}
 		}
-		data := strings.TrimSpace(string(bytes[:i]))
-		parts := strings.Split(data, "\n")
-		clients[strings.Split(parts[2], " = ")[1]] = strings.Split(parts[0], " ")[1]
-	}
-	cmd := exec.Command("sudo", "wg", "show")
-	bytes, err = cmd.Output()
-	if err != nil {
-		return nil, err
-	}
-	var tempUsers []User
-	for _, data := range strings.Split(string(bytes), "\n\n")[1:] {
-		if name, ok := clients[strings.Split(strings.Split(data, "\n")[0], " ")[1]]; ok {
-			ti := strings.Index(data, "transfer")
-			ai := strings.Index(data, "allowed ips")
-			allowedIpsLine := data[ai:]
-			allowedIps := allowedIpsLine[13:strings.Index(allowedIpsLine, "/")]
-			if ti > 0 {
-				line := data[ti:]
-				line = strings.Replace(line, "transfer: ", "", -1)
-				line = strings.Replace(line, " received,", "", -1)
-				line = strings.Replace(line, " sent", "", -1)
-				statsParts := strings.Split(line, " ")
-				tx, _ := strconv.ParseFloat(statsParts[0], 32)
-				if statsParts[1] == "KiB" {
-					tx *= 1000
-				} else if statsParts[1] == "MiB" {
-					tx *= 1000000
-				} else if statsParts[1] == "GiB" {
-					tx *= 1000000000
-				}
-				rx, _ := strconv.ParseFloat(statsParts[2], 32)
-				if statsParts[3] == "KiB" {
-					rx *= 1000
-				} else if statsParts[3] == "MiB" {
-					rx *= 1000000
-				} else if statsParts[3] == "GiB" {
-					rx *= 1000000000
-				}
-				handshakeIndex := strings.Index(data, "latest handshake:")
-				handshakeLine := data[handshakeIndex:]
-				newlineIndex := strings.Index(handshakeLine, "\n")
-				lastestHandshake := handshakeLine[18:newlineIndex]
-				tempUsers = append(tempUsers, User{
-					Name:             name,
-					Tx:               int(tx),
-					Rx:               int(rx),
-					LastestHandshake: lastestHandshake,
-					AllowedIps:       allowedIps,
-				})
-			} else {
-				tempUsers = append(tempUsers, User{
-					Name:             name,
-					Tx:               0,
-					Rx:               0,
-					LastestHandshake: "",
-					AllowedIps:       allowedIps,
-				})
+		peers[info[0]].AllowedIps = string(info[3])
+		peers[info[0]].LatestHandshake, _ = strconv.ParseUint(string(info[4]), 10, 64)
+		peers[info[0]].Rx, _ = strconv.ParseUint(string(info[5]), 10, 64)
+		peers[info[0]].Tx, _ = strconv.ParseUint(string(info[6]), 10, 64)
+		if peers[info[0]].Name == "" {
+			bytes, err := os.ReadFile("/etc/wireguard/wg0.conf")
+			if err != nil {
+				panic(err)
 			}
+			j := strings.Index(string(bytes), "\n[Peer]\nPublicKey = "+info[0])
+			i := j - 1
+			for string(bytes[i]) != " " && i >= 0 {
+				i--
+			}
+			i++
+			peers[info[0]].Name = string(bytes[i:j])
 		}
 	}
-	sort.Slice(tempUsers, func(i, j int) bool {
-		return tempUsers[i].Tx+tempUsers[i].Rx > tempUsers[j].Tx+tempUsers[j].Rx
-	})
-	return tempUsers, nil
 }
 
 func findUserByIp(ip string) string {
-	for i := 0; i < len(users); i++ {
-		if ip == users[i].AllowedIps {
-			return users[i].Name
+	for _, p := range peers {
+		if slices.Contains(strings.Split(p.AllowedIps, ","), ip) {
+			return p.Name
 		}
 	}
 	return ""
 }
 
 func main() {
-	db := Database{}
-	db.Init("db.json")
-	defer db.Commit()
 	admins = os.Args[1:]
 	go func() {
 		cmd := exec.Command("vnstat", "-l")
@@ -188,14 +105,13 @@ func main() {
 	}()
 	go func() {
 		for range time.NewTicker(time.Second).C {
-			users, _ = getUsers()
-			db.Users = users
+			updatePeersInfo()
 		}
 	}()
 	http.Handle("/api", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		name := findUserByIp(strings.Split(r.RemoteAddr, ":")[0])
 		name = strings.Split(name, "-")[0]
-		var temp []User
+		tempPeers := make(map[string]*Peer)
 		isAdmin := false
 		for _, n := range admins {
 			if n == name {
@@ -204,22 +120,22 @@ func main() {
 			}
 		}
 		if isAdmin {
-			temp = users
+			tempPeers = peers
 		} else {
-			for _, u := range users {
-				if strings.Contains(u.Name, name) {
-					temp = append(temp, u)
+			for pk, p := range peers {
+				if strings.Contains(p.Name, name) {
+					tempPeers[pk] = p
 				}
 			}
 		}
 		data := make(map[string]interface{})
-		data["Users"] = temp
+		data["Users"] = tempPeers
 		data["Rx"] = Rx
 		data["Tx"] = Tx
 		data["IsAdmin"] = isAdmin
 		bytes, err := json.Marshal(data)
 		if err != nil {
-			w.Write([]byte("[]"))
+			w.Write([]byte("{}"))
 			return
 		}
 		w.Header().Add("Access-Control-Allow-Origin", "*")
